99cf1f667b03251cfe1c7a29f9f40d8b65ed16a5
David Cano Marquez <david.canomarquez@students.hevs.ch>
2018-03-21 20:58:47 +0100

    david 3

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 # Setup
-#lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R . public_html"
+lftp -c "open -u David,3666 ftp://81.221.244.152/USB-FlashDisk-01/"
